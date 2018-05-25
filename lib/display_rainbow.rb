90b043766fd9ce27bc133938a6ab7c6bd5c2e738
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-25 14:32:02 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 
-colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
 uppercase = ['R', 'O', 'Y', 'G', 'B', 'I', 'V']
 
 def display_rainbow(colours) 
